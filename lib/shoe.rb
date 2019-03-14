edb94babb7e7950969dc7c06691dc403ac1fdbc4
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-14 20:14:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,15 +5,18 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+
   def brand(=brand)
     @brand = brand
     @size = size
     BRANDS << brand
   end
+
    def each_brand(brand)
      @BRANDS.each do |brand|
      end
    end
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
