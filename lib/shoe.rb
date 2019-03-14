019de3df5c79a0e46157d7135f697928d9417d12
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-14 20:09:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
+  
   end
    def each_brand(brand)
      @BRANDS.each do |brand|
